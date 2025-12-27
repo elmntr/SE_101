cfb2b23f3f48a4bf2a2784d5a4ee43035f2dbb7d
spooder983 <spooder983@gmail.com>
2025-12-27 21:38:13 +0800

    wala ito, nagdidelte lng, putaina

--- android/app/src/main/kotlin/com/example/chickenjoo_inventory/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.chickenjoo_inventory
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/database/daos/roles_dao.g.dart
+++
@@ -1,8 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'roles_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$RolesDaoMixin on DatabaseAccessor<AppDatabase> {
-  $RolesTable get roles => attachedDatabase.roles;
-}
